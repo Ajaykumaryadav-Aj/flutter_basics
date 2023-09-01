6ddd72646ee4253affc2b0156fa66894b05cf947
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2023-09-01 23:29:26 +0530

    initial commit

--- my_first_app/lib/mydesign/my_ui_design.dart
+++ my_first_app/lib/mydesign/my_ui_design.dart
@@ -5,48 +5,73 @@ class MyUiDesign extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      // backgroundColor: Colors.amber,
-      body: Center(
-        child: Column(
+    return Scaffold(
+      // ignore: prefer_const_constructors
+      appBar: AppBar(
+        actions: const [Icon(Icons.widgets), Icon(Icons.circle),],
+        title: const Column(
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(13, 50, 13, 0),
-              child: ListTile(
-                title: Text('Welcome Back', style: TextStyle(fontSize: 13)),
-                subtitle: Text(
-                  'Tony Stark',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                trailing: Wrap(
-                  spacing: 10.0,
-                  children: [
-                    Icon(Icons.circle_rounded),
-                    Icon(
-                      Icons.widgets,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            // RichText(
-            //   text: const TextSpan(
-            //       text: 'abbbbbbbbb',
-            //       style: TextStyle(fontSize: 70.0, color: Colors.amber),
-            //       children: [TextSpan(text: 'aaaaaaaaaaaaaaaaaaaaaay')]),
-            // ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 60, 130, 0),
-              child: Text(
-                'Find Best\nMentors For\nYou',
-                style: TextStyle(fontSize: 40.0),
-              ),
-            ),
-
-  
+            Text('Welcome back'),
+            Text('Tonny Stark'),
           ],
         ),
       ),
     );
   }
 }
+
+        // child: Column(
+        //   children: [
+        //     Padding(
+        //       padding: EdgeInsets.fromLTRB(13, 50, 13, 0),
+        //       child: ListTile(
+        //         title: Text('Welcome Back', style: TextStyle(fontSize: 13)),
+        //         subtitle: Text(
+        //           'Tony Stark',
+        //           style: TextStyle(fontSize: 20.0),
+        //         ),
+        //         trailing: Wrap(
+        //           spacing: 10.0,
+        //           children: [
+        //             Icon(Icons.circle_rounded),
+        //             Icon(
+        //               Icons.widgets,
+        //             ),
+        //           ],
+        //         ),
+        //       ),
+        //     ),
+            // RichText(
+            //   text: const TextSpan(
+            //       text: 'abbbbbbbbb',
+            //       style: TextStyle(fontSize: 70.0, color: Colors.amber),
+            //       children: [TextSpan(text: 'aaaaaaaaaaaaaaaaaaaaaay')]),
+            // ),
+            // Padding(
+            //   padding: EdgeInsets.fromLTRB(0, 60, 130, 0),
+            //   child: Text(
+            //     'Find Best\nMentors For\nYou',
+            //     style: TextStyle(fontSize: 40.0),
+            //   ),
+            // ),
+            // Stack(
+            //   clipBehavior: Clip.none,
+            //   children: [
+            //     CircleAvatar(
+            //       backgroundColor: Colors.amber,
+            //     ),
+            //     Positioned(
+            //       left: 20,
+            //       child: CircleAvatar(
+            //         backgroundColor: Colors.blue,
+            //       ),
+            //     ),
+            //     Positioned(
+            //       left: 40,
+            //       child: CircleAvatar(
+            //         backgroundColor: Colors.black12,
+            //       ),
+            //     )
+            //   ],
+            // )
+          
