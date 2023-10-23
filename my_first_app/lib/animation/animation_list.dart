08590031448fa061ad69f3cf628d020a0cf7bbec
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2023-10-24 02:56:04 +0530

    Animated list

---
+++ my_first_app/lib/animation/animation_list.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedListt extends StatefulWidget {
+  const AnimatedListt({super.key});
+
+  @override
+  State<AnimatedListt> createState() => _AnimatedListtState();
+}
+
+class _AnimatedListtState extends State<AnimatedListt> {
+  final _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+
+  void _addItems() {
+    _items.insert(0, 'Item${_items.length + 1}');
+    _key.currentState!.insertItem(0, duration: const Duration(seconds: 1));
+  }
+
+  // final mytile = ListTile();
+
+  void _removeItems(int index) {
+    _items.removeAt(index);
+    _key.currentState!.removeItem(
+        index, (context, animation) => SizeTransition(sizeFactor: animation));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated list'),
+      ),
+      body: AnimatedList(
+        key: _key,
+        initialItemCount: _items.length,
+        itemBuilder: (context, index, animation) {
+          return SizeTransition(
+            sizeFactor: animation,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  _items[index],
+                  style: const TextStyle(color: Colors.white),
+                ),
+                tileColor: Colors.indigo,
+                trailing: IconButton(
+                    onPressed: () => _removeItems(index),
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    )),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addItems,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_first_app/animation/animation_list.dart';
 import 'package:my_first_app/animation/curve_navigatiobar_screen.dart';
 import 'package:my_first_app/firebase_options.dart';
-
 
 final messangerkey = GlobalKey<ScaffoldMessengerState>();
 
@@ -35,44 +35,44 @@ class MyApp extends StatelessWidget {
         // ],
         // child:
         MaterialApp(
-      scaffoldMessengerKey: messangerkey,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
-        useMaterial3: true,
-        //   colorScheme: ColorScheme.fromSeed(
-        //     seedColor: const Color.fromRGBO(36, 97, 220, 1),
-        //   ),
-      ),
+            scaffoldMessengerKey: messangerkey,
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+              useMaterial3: true,
+              //   colorScheme: ColorScheme.fromSeed(
+              //     seedColor: const Color.fromRGBO(36, 97, 220, 1),
+              //   ),
+            ),
 
-      // home: const MyHomeScreen(),
-      // home: const MyWhatsapp(),
-      // home: const ProductScreen(),
-      // home: const SelectorScreen(),
-      // home: const MyUiDesign(),
-      // home: const SliverScreen(),
-      // home: const Googlekeeps(),
-      // home: const ApiUserScreen(),
-      // home: const CountScreens(),
-      // home: const ApiUserScreen(),
-      // home: const RandomUserScreen(),
-      // home: const PostScreen(),
-      // home: const FormScreen(),
-      // home: const PickerScreen(),
-      // home: const Advancedwidgets(),
-      // home: const ListviewScreen(),
-      // home: const Rotatedbox(),
-      // home: const PageviewScreen(),
-      // home: const PaintScreen(),
-      // home: const CliperScreeen(),
-      // home: const MaterialScreen(),
-      // home: const PaginationScreen(),
-      // home: const PaginationScreen(),
-      // home: const (),
-      // home: const HiveTodoScreen(),
-      // home: const InitialScreen(),
-      // home: const RandomUserScreen(),
-      home: const CurvenNavigation(),
-    );
+            // home: const MyHomeScreen(),
+            // home: const MyWhatsapp(),
+            // home: const ProductScreen(),
+            // home: const SelectorScreen(),
+            // home: const MyUiDesign(),
+            // home: const SliverScreen(),
+            // home: const Googlekeeps(),
+            // home: const ApiUserScreen(),
+            // home: const CountScreens(),
+            // home: const ApiUserScreen(),
+            // home: const RandomUserScreen(),
+            // home: const PostScreen(),
+            // home: const FormScreen(),
+            // home: const PickerScreen(),
+            // home: const Advancedwidgets(),
+            // home: const ListviewScreen(),
+            // home: const Rotatedbox(),
+            // home: const PageviewScreen(),
+            // home: const PaintScreen(),
+            // home: const CliperScreeen(),
+            // home: const MaterialScreen(),
+            // home: const PaginationScreen(),
+            // home: const PaginationScreen(),
+            // home: const (),
+            // home: const HiveTodoScreen(),
+            // home: const InitialScreen(),
+            // home: const RandomUserScreen(),
+            home:const AnimatedListt()
+            );
   }
 }
